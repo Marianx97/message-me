a70b393ce8c4cf806d26fe7db3bacd61a5c81896
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-19 19:28:50 -0300

    Created User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts '### creating Users ###'
+
+User.create(username: "Albert", password: "BetoBeto")
+User.create(username: "Barbara", password: "AquaGirl")
+User.create(username: "Connie", password: "Conmincon")
+User.create(username: "David", password: "Dman237")
+User.create(username: "Esther", password: "EttyPass12")
