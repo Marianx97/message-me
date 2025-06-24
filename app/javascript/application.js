5bd48d1925834181d2c7aa73914996b563acbf2f
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-24 13:46:17 -0300

    Added action cable functionality for real time messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import * as jquery from "jquery"
 import "semantic-ui"
+import "channels"
+
+function scroll_bottom() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+};
 
 $(document).on('turbo:load', function() {
   console.log('loaded turbo links')
@@ -10,4 +17,5 @@ $(document).on('turbo:load', function() {
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom()
 });
